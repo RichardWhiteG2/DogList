0206708adf8dd87a09485220d5126d42e783a16d
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-10-04 00:09:39 -0500

    Creacion del API Rest, falta crear el RecyclerView

--- app/src/main/java/com/ricardoblanco/doglist/MainActivity.kt
+++ app/src/main/java/com/ricardoblanco/doglist/MainActivity.kt
@@ -2,10 +2,37 @@ package com.ricardoblanco.doglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ricardoblanco.doglist.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    private fun searchByName(query:String){
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
+            val puppies = call.body()
+            if(call.isSuccessful){
+                //show Recyclerview
+            }else{
+                //show error
+            }
+        }
     }
 }
